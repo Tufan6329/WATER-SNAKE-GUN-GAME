3803f1c9248883b433c7dc7c74faadc66c126470
CHIRAG KAMLESHBHAI JOTANIYA <112388120+Tufan6329@users.noreply.github.com>
2022-12-05 11:15:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+
+def gamewin(comp,you ):
+    if comp==you:
+        return None
+    elif comp=='s':
+        if you=='w':
+             return False
+        elif you=='g':
+            return True
+    elif comp=='w':
+        if you=='g':
+             return False
+        elif you=='s':
+            return True
+    elif comp=='g':
+        if you=='s':
+             return False
+        elif you=='w':
+            return True
+
+print("comp turn:snake(s) water(w) or gun(g)?")
+randno=random.randint(1,3)
+print(randno)
+if randno==1:
+    comp='s'
+elif randno==2:
+    comp='w'
+elif randno==3:
+    comp='g'
+
+you=input("player :snake(s) water(w) or gun(g)?")
+
+a=gamewin(comp,you)
+
+print(f"computer choose  {comp}")
+print(f"computer choose   {you}")
+if a == None:
+    print("the game is tie")
+elif a:
+    print("the winner")
+else:
+    print("you lose")
